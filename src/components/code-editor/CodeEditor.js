9445bc638e538afe472d18ea014c24b154e37c48
Vladimir Popov <vladapopov02@gmail.com>
2024-03-02 19:10:18 +0100

    feat: implement file upload for code editor

--- src/components/code-editor/CodeEditor.js
+++ src/components/code-editor/CodeEditor.js
@@ -1,10 +1,44 @@
 import { Editor } from "@monaco-editor/react";
+import { Button } from "../button/Button";
+import "./CodeEditor.css";
+import { useRef, useState } from "react";
 
 function CodeEditor() {
+  const hiddenFileInput = useRef(null);
+  const [fileContent, setFileContent] = useState("");
+  let fileReader;
+
+  const handleClick = () => {
+    hiddenFileInput.current.click();
+  };
+
+  const handleFileRead = (e) => {
+    const content = fileReader.result;
+    setFileContent(content);
+  };
+
+  const handleFile = (file) => {
+    fileReader = new FileReader();
+    fileReader.onloadend = handleFileRead;
+    fileReader.readAsText(file);
+  };
+
+  const handleChange = (event) => {
+    const fileUploaded = event.target.files[0];
+    handleFile(fileUploaded);
+  };
+
   return (
     <div>
-      <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 p-6">Code Editor</h2>
-      <Editor height="90vh" defaultLanguage="kotlin" />
+      <div className="line">
+        <h2 className="scroll-m-20 text-3xl font-semibold tracking-tight first:mt-0 p-6">Code Editor</h2>
+        <Button onClick={handleClick} variant="secondary">
+          Upload a File
+        </Button>
+        <input ref={hiddenFileInput} multiple={false} type="file" onChange={handleChange} style={{ display: "none" }} />
+      </div>
+      <Editor height="83vh" defaultLanguage="kotlin" value={fileContent} />
+      <Button className="submit-btn">Submit</Button>
     </div>
   );
 }
